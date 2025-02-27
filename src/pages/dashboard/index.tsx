80ca6976c1873d64a415cc509d8f77f595af3c4a
sfgardev <sf.garol@gmail.com>
2025-02-27 14:33:59 +0700

    feat: create search input

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,14 @@
 import { Container } from '../../shared/ui/container'
 import { Title } from '../../shared/ui/title'
 import { Layout } from '../../shared/ui/layout'
+import { SearchInput } from '../../features/search-input'
 
 const Dashboard = () => {
   return (
     <Layout>
       <Container>
         <Title>Dashboard</Title>
+        <SearchInput placeholder="What test are you looking for?" resultsCount={2} />
       </Container>
     </Layout>
   )
