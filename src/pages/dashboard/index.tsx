51635bdf14f4dea807a0c7d17cbee7635bf4b888
sfgardev <sf.garol@gmail.com>
2025-02-27 20:04:51 +0700

    feat: dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,13 +2,19 @@ import { Container } from '../../shared/ui/container'
 import { Title } from '../../shared/ui/title'
 import { Layout } from '../../shared/ui/layout'
 import { SearchInput } from '../../features/search-input'
+import { Table } from '../../widgets/table'
+import s from './dashboard.module.scss'
 
 const Dashboard = () => {
   return (
     <Layout>
       <Container>
-        <Title>Dashboard</Title>
-        <SearchInput placeholder="What test are you looking for?" resultsCount={2} />
+        <Title className={s.title}>Dashboard</Title>
+        <SearchInput
+          placeholder="What test are you looking for?"
+          resultsCount={2}
+        />
+        <Table className={s.table} />
       </Container>
     </Layout>
   )
