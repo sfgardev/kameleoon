e8375a710585e52b67bed40f42694fab1a2dcab0
sfgardev <sf.garol@gmail.com>
2025-02-28 12:16:56 +0700

    feat: add goBack button to results and finalize pages

--- src/pages/finalize/index.tsx
+++ src/pages/finalize/index.tsx
@@ -1,19 +1,23 @@
-import { useParams } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
 import { useTestById } from '../../entities/test'
 import { Layout } from '../../shared/ui/layout'
 import { Container } from '../../shared/ui/container'
 import { Title } from '../../shared/ui/title'
 import { Subtitle } from '../../shared/ui/subtitle'
+import { GoBackButton } from '../../shared/ui/go-back-button'
+import s from '../results/results.module.scss'
 
 const Finalize = () => {
   const { testId } = useParams()
+  const navigate = useNavigate()
   const { testName } = useTestById(testId)
 
   return (
     <Layout>
-      <Container>
+      <Container className={s.container}>
         <Title>Finalize</Title>
         <Subtitle>{testName}</Subtitle>
+        <GoBackButton onClick={() => navigate(-1)} className={s.goBackButton} />
       </Container>
     </Layout>
   )
